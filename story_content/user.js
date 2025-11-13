5ae13385e904c664dc11e7454c5a40818321ab3d
ab-alharthi <ab.alharthi25@gmail.com>
2025-11-13 03:38:04 +0300

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  document.querySelectorAll('iframe').forEach(f => {
+  f.setAttribute('allow', 'camera; microphone;');
+});
+}
+
+};
